4c7f74940d9eeec172d149bfb2e6ad768f590d3e
OlegSuprunovich <olsupr@ukr.net>
2022-07-07 22:44:20 +0300

    Add celsius fahrenheit change

--- src/index.js
+++ src/index.js
@@ -35,7 +35,10 @@ function displayWeatherCondition(response) {
     document.querySelector("#temperature").innerHTML = Math.round(
         response.data.main.temp
     );
+    let temperatureElement = document.querySelector("#temperature");
+    celsiusTemperature = response.data.main.temp;
 
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
     document.querySelector("#wind").innerHTML = Math.round(
         response.data.wind.speed
@@ -76,3 +79,34 @@ function getCurrentLocation(event) {
 }
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
+
+
+
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+
+    celsiusLink.classList.remove("nonlink");
+    fahrenheitLink.classList.add("nonlink");
+    let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    celsiusLink.classList.add("nonlink");
+    fahrenheitLink.classList.remove("nonlink");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature)
